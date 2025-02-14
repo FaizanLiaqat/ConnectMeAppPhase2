9327afffe6a5d00b2d06380fb6bfe97e1543b8c7
FaizanLiaqat <faizanliaqat192@gmail.com>
2025-02-14 10:03:19 +0500

    first screen

---
+++ app/src/main/java/com/example/connectmeapp/FirstScreen.kt
@@ -0,0 +1,29 @@
+package com.example.connectmeapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class FirstScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_first_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        lifecycleScope.launch {
+            delay(1000) // Wait for 3 seconds
+            startActivity(Intent(this@FirstScreen, SecondScreen::class.java))
+            finish()
+        }
+    }
+}
